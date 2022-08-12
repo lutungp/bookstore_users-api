3cd3e992e85a137e0167d430258a38545fa3a93c
lintang permana <lntngp19@gmail.com>
2022-08-12 10:02:08 +0700

    How to design & develop REST microservices in Golang (Go)

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -10,30 +10,27 @@ import (
 
 const (
 	indexUniqueEmail = "email_UNIQUE"
+	errorNoRows      = "no rows in result set"
 	queryInserUser   = "INSERT INTO users (first_name, last_name, email, date_created) VALUES (?, ?, ?, ?);"
-)
-
-var (
-	usersDB = make(map[int64]*User)
+	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users where id=?"
 )
 
 func (user *User) Get() *errors.RestErr {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
 
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	defer stmt.Close()
+
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError(fmt.Sprintf("user %d does not found", user.Id))
+		}
+		fmt.Println(err)
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()))
 	}
-
-	fmt.Println(result)
-
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
 
 	return nil
 }
@@ -51,7 +48,7 @@ func (user *User) Save() *errors.RestErr {
 	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
 	if err != nil {
 		if strings.Contains(err.Error(), indexUniqueEmail) {
-			return errors.NewInternalServerError(fmt.Sprintf("email %s already exist", user.Email))
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exist", user.Email))
 		}
 		return errors.NewInternalServerError(fmt.Sprintf("error when try to save user: %s", err.Error()))
 	}
