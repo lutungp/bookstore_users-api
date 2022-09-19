7318f9549fa9d36ec0e53ffaed1868b34eafad42
lintang permana <lntngp19@gmail.com>
2022-09-19 12:59:06 +0700

    17. How to marshal structs

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -34,7 +34,7 @@ func Get(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, user)
+	c.JSON(http.StatusOK, user.Marshall(c.GetHeader("X-Public") == "true"))
 }
 
 func Create(c *gin.Context) {
@@ -53,7 +53,7 @@ func Create(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusCreated, result)
+	c.JSON(http.StatusCreated, result.Marshall(c.GetHeader("X-Public") == "true"))
 }
 
 func Update(c *gin.Context) {
@@ -81,7 +81,7 @@ func Update(c *gin.Context) {
 		c.JSON(err.Status, err)
 		return
 	}
-	c.JSON(http.StatusOK, result)
+	c.JSON(http.StatusOK, result.Marshall(c.GetHeader("X-Public") == "true"))
 }
 
 func Delete(c *gin.Context) {
@@ -107,5 +107,6 @@ func Search(c *gin.Context) {
 		c.JSON(err.Status, err)
 		return
 	}
-	c.JSON(http.StatusOK, users)
+
+	c.JSON(http.StatusOK, users.Marshall(c.GetHeader("X-public") == "true"))
 }
--- services/users_service.go
+++ services/users_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"github/lutungp/bookstore_users-api/domain/users"
+	"github/lutungp/bookstore_users-api/utils/crypto_utils"
 	"github/lutungp/bookstore_users-api/utils/date_utils"
 	"github/lutungp/bookstore_users-api/utils/errors"
 )
@@ -13,6 +14,7 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 
 	user.Status = users.StatusActive
 	user.DateCreated = date_utils.GetNowDBFormat()
+	user.Password = crypto_utils.GetMd5(user.Password)
 	if err := user.Save(); err != nil {
 		return nil, err
 	}
@@ -65,7 +67,7 @@ func DeleteUser(userId int64) *errors.RestErr {
 	return user.Delete()
 }
 
-func Search(status string) ([]users.User, *errors.RestErr) {
+func Search(status string) (users.Users, *errors.RestErr) {
 	dao := &users.User{}
 	return dao.FindByStatus(status)
 }
